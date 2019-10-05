2d63741c0264d1b0b7ee9ec0a97196a3e53ac814
Jostein Hamre <josteinhamre@gmail.com>
2019-10-06 01:28:29 +0200

    Fixed links in text.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,13 @@ let tabcontent = document.querySelectorAll(".tabcontent");
 tablinks.forEach ((tab) => {
   tab.addEventListener("click", (event) => {
     document.querySelectorAll('.active').forEach ((active) => {
-      active.classList.remove('active')
-    })
-    event.target.classList.add('active')
-    document.getElementById(event.target.dataset.tab).classList.add('active')
+      active.classList.remove('active');
+    });
+    event.target.classList.add('active');
+    document.getElementById(event.target.dataset.tab).classList.add('active');
+      if (event.target.dataset.tab == 'info') {
+    document.documentElement.scrollTop = 0;
+    };
   });
 
-})
+});
